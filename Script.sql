bd9f9381871a8c0e7b1cfb8bffb3dd6ea754997f
Ricardo Almeida de Souza <ricksouz96@gmail.com>
2024-05-29 19:59:09 -0300

    Adiciona o script de solução para o desafio

---
+++ Script.sql
@@ -0,0 +1,65 @@
+--1
+SELECT Nome, Ano
+FROM Filmes
+
+--2
+SELECT Nome, Ano
+FROM Filmes
+ORDER BY Ano 
+
+--3
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+--4
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano = 1997
+
+--5
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano > 2000
+
+--6
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Duracao
+BETWEEN 101 AND 149
+ORDER BY Duracao 
+
+--7
+SELECT Ano, COUNT(*) FROM Filmes
+GROUP BY Ano
+ORDER BY SUM(Duracao) DESC 
+
+--8
+SELECT PrimeiroNome, UltimoNome, Genero
+FROM Atores
+WHERE Genero = 'M'
+
+--9
+SELECT PrimeiroNome, UltimoNome, Genero
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+--10
+SELECT Nome, Genero
+FROM Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+--11
+SELECT Nome, Genero
+FROM Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+--12
+SELECT Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel
+FROM Filmes
+INNER JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
+INNER JOIN Atores ON ElencoFilme.IdAtor = Atores.Id
